d889fb7fd8460169198be45f239896ab4c179f4e
angcallla <angcallla@alum.us.es>
2019-12-29 20:59:35 +0100

    Archivo con el modelo de BeautifulSoup
    Debería servir para todas las categorías. Todavía se le debe dar un apaño de tal manera que dado el enlace de la pagina te devuelva los atributos de cada libro o algo así

---
+++ sbooks/beautifulSoupModel.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+import sqlite3
+import time
+
+from bs4 import BeautifulSoup
+import requests
+import urllib
+import os
+import re
+
+
+def modelo_beautifulsoup(enlace):  # Imprime por consola los resultados de la primera página
+    my_html_text = requests.get(enlace)
+    soup = BeautifulSoup(my_html_text.text, "html.parser")
+    # FAMILIA - CATEGORIA - SUBCATEGORIA
+    categoria = ""
+    subcategoria = ""
+    ul = soup.find("ul", id="breadcrumb-flow")
+    if ul is not None:
+        li = ul.find_all("li")
+        categoria = li[len(li) - 2].a.div.string
+        subcategoria = li[len(li) - 1].div.string
+    print("Categoria: " + categoria + "\n" + "Subcategoria: " + subcategoria)
+
+    # LISTA DE LIBROS
+    resultados_pagina = soup.find("div", class_="results-page")  # Buscando esto primero filtramos los libros de arriba
+    libros_lista = resultados_pagina.find_all("div", class_="product")
+    i = 0
+    for x in libros_lista:
+        i = i + 1
+        print("*** Libro " + str(i) + " ***")
+
+        # TITULO
+        titulo = x.find("a", class_="title")
+        if titulo is not None:
+            titulo = titulo.string
+        else:
+            titulo = ""
+        print("Titulo: " + titulo)
+
+        # AUTOR
+        div_author = x.find("div", class_="author")
+        if div_author is not None:
+            try:
+                autor = div_author.a.string
+            except:
+                autor = div_author.div.string
+        else:
+            autor = ""
+        print("Autor: " + autor)
+
+        # BREVE DESCRIPCION
+        breve_descripcion = x.find("div", class_="resumen")
+        if breve_descripcion is not None:
+            breve_descripcion = breve_descripcion.text
+        else:
+            breve_descripcion = ""
+        print("Breve Descripcion: " + breve_descripcion)
+
+        # FOTO PORTADA
+        foto_portada = x.find("img", class_="v-lazy-image")
+        if foto_portada is not None:
+            foto_portada = foto_portada["data-src"]
+        else:
+            foto_portada = ""
+        print("Foto Portada (enlace): " + foto_portada)
+        print("------------------------------------")
+
+
+if __name__ == "__main__":
+    modelo_beautifulsoup("https://www.casadellibro.com/libros/ciencias-humanas/filosofia/104003000")
